ab8249c58cbb9b92cfe2614394531d1b8562146e
elengreen <elen_november@mail.ru>
2022-11-09 00:30:34 +0700

    Added departmentChoice page

---
+++ pages/departmentChoice/render.js
@@ -0,0 +1,31 @@
+function clickBackBtn() {
+    window.location.href = "../settings/settings.html";
+}
+
+async function clickApplyBtn() {
+
+}
+
+(async() => {
+    let filter = await data.getFilter();
+    let alldata = await data.all();
+    let averData = data.avarageByKeys(alldata);
+
+    console.log(filter);
+    console.log(data.avarageByKeys(alldata));
+
+    var config = {};
+
+    config.segments = await wheelService.getConfigSegments(averData);
+
+    config.radius = 200; // optional. Default calculated based in canvas size
+    config.levels = 10; // optional. Default 10
+    config.fontSize = 15; // optional. Default 15px
+
+    let canvas = document.getElementById("canvas");
+
+    const wheel = new Wheel(canvas, config);
+    wheel.draw();
+
+    canvas.click(false);
+})();
