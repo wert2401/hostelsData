e575c575cc2029e7436d5c214dacbd0119a8d563
elengreen <elen_november@mail.ru>
2022-11-09 23:50:07 +0700

    Added quarterWheels page

---
+++ pages/quarterWheels/render.js
@@ -0,0 +1,45 @@
+function clickBackBtn() {
+    window.location.href = "../periodChoice/periodChoice.html";
+}
+
+async function clickApplyBtn() {
+
+}
+
+(async() => {
+    let filter = await data.getFilter();
+    let alldata = await data.all();
+    let averData = data.avarageByKeys(alldata);
+
+    console.log(filter);
+    console.log(data.avarageByKeys(alldata));
+
+    var config = {};
+
+    config.segments = await wheelService.getConfigSegments(averData);
+
+    config.radius = 200; // optional. Default calculated based in canvas size
+    config.levels = 10; // optional. Default 10
+    config.fontSize = 15; // optional. Default 15px
+
+    let canvas1 = document.getElementById("canvas1");
+
+    const wheel1 = new Wheel(canvas1, config);
+    wheel1.draw();
+
+    canvas1.click(false);
+
+    let canvas2 = document.getElementById("canvas2");
+
+    const wheel2 = new Wheel(canvas2, config);
+    wheel2.draw();
+
+    canvas2.click(false);
+
+    let canvas3 = document.getElementById("canvas3");
+
+    const wheel3 = new Wheel(canvas3, config);
+    wheel3.draw();
+
+    canvas3.click(false);
+})();
